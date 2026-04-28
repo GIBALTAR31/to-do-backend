9cb52d2f8a0273b9b3b86fdb461bf11974508c69
GIBALTAR31 <atargibaltar@gmail.com>
2026-04-28 21:40:20 +0700

    feat: add UpdateTodoById handler, repository, and main

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -85,7 +85,7 @@ func GetTodoByID (pool *pgxpool.Pool, id int) (*models.Todo, error) {
 	defer cancel()
 
 	var query string = `
-		SELECT * FROM todos WHERE id = $1
+		SELECT id, title, completed, created_at, updated_at FROM todos WHERE id = $1
 	`
 	var todo models.Todo
 	var err = pool.QueryRow(ctx, query, id).Scan(
@@ -96,6 +96,37 @@ func GetTodoByID (pool *pgxpool.Pool, id int) (*models.Todo, error) {
 		&todo.UpdatedAt,
 	)
 	if err != nil {
+		return nil, err
+	}
+
+	return &todo, nil
+}
+
+func UpdateTodoByID(pool *pgxpool.Pool, id int, title string, completed bool) (*models.Todo, error){
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5 *time.Second)
+	defer cancel()
+
+	var query string = `
+		UPDATE todos
+		SET title = $1, completed = $2, updated_at = CURRENT_TIMESTAMP
+		WHERE id = $3
+		RETURNING id, title, completed, created_at, updated_at
+	`
+
+	var todo models.Todo
+
+	var err error = pool.QueryRow(ctx, query, title, completed, id).Scan(
+		&todo.ID,
+		&todo.Title,
+		&todo.Completed,
+		&todo.CreatedAt,
+		&todo.UpdatedAt,
+	)
+
+	if (err !=  nil){
 		return nil, err
 	}
 
